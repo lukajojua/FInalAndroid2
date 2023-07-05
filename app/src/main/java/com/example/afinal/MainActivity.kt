d28d5649fcdf3af3b6c5dc94d6e8d1353d7f834b
lukajojua <luka.jojua@azry.com>
2023-07-06 01:33:18 +0400

    final project

--- app/src/main/java/com/example/afinal/MainActivity.kt
+++ app/src/main/java/com/example/afinal/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.example.afinal
 
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.provider.MediaStore
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.afinal.ml.MobilenetV110224Quant
+import com.example.afinal.utils.Constants.CAMERA_REQUEST
+import com.example.afinal.utils.Constants.SELECT_PICTURE_REQUEST
 import org.tensorflow.lite.DataType
 import org.tensorflow.lite.support.image.ImageProcessor
 import org.tensorflow.lite.support.image.TensorImage
@@ -16,68 +22,86 @@ import org.tensorflow.lite.support.image.ops.ResizeOp
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var selectBtn: Button
-    private lateinit var predictionBtn: Button
-    private lateinit var resultView: TextView
+    private lateinit var selectButton: Button
+    private lateinit var predictButton: Button
+    private lateinit var cameraButton: Button
+    private lateinit var resultTextView: TextView
     private lateinit var imageView: ImageView
-    private lateinit var bitmap: Bitmap
-    private lateinit var labels: List<String>
-
+    private lateinit var imageBitmap: Bitmap
+    private lateinit var labelList: List<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Base_Theme_FInal)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
-        selectBtn = findViewById(R.id.select_btn)
-        predictionBtn = findViewById(R.id.predict_btn)
-        resultView = findViewById(R.id.result_view)
+        selectButton = findViewById(R.id.select_btn)
+        predictButton = findViewById(R.id.predict_btn)
+        cameraButton = findViewById(R.id.take_picture)
+        resultTextView = findViewById(R.id.result_view)
         imageView = findViewById(R.id.image_view)
-        val imageProcessor =
-            ImageProcessor.Builder()
-                .add(ResizeOp(224, 224, ResizeOp.ResizeMethod.BILINEAR))
-                .build()
 
-        labels = application.assets.open("label.txt").bufferedReader().readLines()
+        val imageProcessor = ImageProcessor.Builder()
+            .add(ResizeOp(224, 224, ResizeOp.ResizeMethod.BILINEAR))
+            .build()
 
+        labelList = application.assets.open("label.txt").bufferedReader().readLines()
 
-        selectBtn.setOnClickListener {
-            val intent: Intent = Intent()
+        cameraButton.setOnClickListener {
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(cameraIntent, CAMERA_REQUEST)
+        }
+
+        selectButton.setOnClickListener {
+            val intent = Intent()
             intent.action = Intent.ACTION_GET_CONTENT
             intent.type = "image/*"
-            startActivityForResult(intent, 200)
+            startActivityForResult(intent, SELECT_PICTURE_REQUEST)
         }
-        predictionBtn.setOnClickListener {
-            var tensorImage = TensorImage(DataType.UINT8)
-            tensorImage.load(bitmap)
-            tensorImage = imageProcessor.process(tensorImage)
-            val model = MobilenetV110224Quant.newInstance(this)
-            val inputFeature0 =
-                TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.UINT8)
-            inputFeature0.loadBuffer(tensorImage.buffer)
-            val outputs = model.process(inputFeature0)
-            val outputFeature0 = outputs.outputFeature0AsTensorBuffer.floatArray
-            var maxindex = 0
-            outputFeature0.forEachIndexed { index, fl ->
-                if (outputFeature0[0] < fl) {
-                    maxindex = index
+
+        predictButton.setOnClickListener {
+            Thread {
+                val (prediction) = predict(imageProcessor, imageBitmap)
+                Handler(Looper.getMainLooper()).post {
+                    resultTextView.text = "i am pretty sure it is $prediction"
                 }
-            }
-            resultView.setText(labels[maxindex])
-
-
-            model.close()
-
+            }.start()
         }
+    }
+
+    private fun predict(imageProcessor: ImageProcessor, bitmap: Bitmap): Pair<String, Float> {
+        var tensorImage = TensorImage(DataType.UINT8)
+        tensorImage.load(bitmap)
+        tensorImage = imageProcessor.process(tensorImage)
+
+        val model = MobilenetV110224Quant.newInstance(this)
+        val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.UINT8)
+        inputFeature0.loadBuffer(tensorImage.buffer)
+
+        val outputs = model.process(inputFeature0)
+        val outputFeature0 = outputs.outputFeature0AsTensorBuffer.floatArray
+
+        var maxIndex = 0
+        var maxConfidence = outputFeature0[0]
+        outputFeature0.forEachIndexed { index, fl ->
+            if (maxConfidence < fl) {
+                maxConfidence = fl
+                maxIndex = index
+            }
+        }
+        model.close()
+        return Pair(labelList[maxIndex], maxConfidence)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == 200) {
-            var uri = data?.data
-            bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, uri)
-            imageView.setImageBitmap(bitmap)
+        if (requestCode == SELECT_PICTURE_REQUEST) {
+            val uri = data?.data
+            imageBitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, uri)
+            imageView.setImageBitmap(imageBitmap)
+        } else if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            imageBitmap = data?.extras?.get("data") as Bitmap
+            imageView.setImageBitmap(imageBitmap)
         }
     }
 }
---
+++ app/src/main/java/com/example/afinal/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.afinal.utils
+
+object Constants {
+    const val SELECT_PICTURE_REQUEST = 200
+    const val CAMERA_REQUEST = 201
+}
